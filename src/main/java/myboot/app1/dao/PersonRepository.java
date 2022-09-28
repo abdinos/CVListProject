5045f183cc4dba418087f1e8bbfeb52778ae4ed5
a18013526 <yann.amrouche@gmail.com>
2022-09-28 19:32:29 +0200

    personService

--- src/main/java/myboot/app1/dao/PersonRepository.java
+++ src/main/java/myboot/app1/dao/PersonRepository.java
@@ -2,13 +2,14 @@ package myboot.app1.dao;
 
 import myboot.app1.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
-public interface UserRepository extends JpaRepository<Person,Long> {
+public interface PersonRepository extends CrudRepository<Person,Long> {
 
 }
 
