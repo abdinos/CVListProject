92fe8aad244607f9ea52bf7a7a086b298ec1652f
Yanis Amrouche <54064644+yanisamrouche@users.noreply.github.com>
2022-11-06 18:41:48 +0100

    API Rest sur les activités

--- src/main/java/myboot/app1/dao/ActivityRepository.java
+++ src/main/java/myboot/app1/dao/ActivityRepository.java
@@ -1,13 +1,18 @@
 package myboot.app1.dao;
 
 import myboot.app1.model.Activity;
+import myboot.app1.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity,Integer> {
+    @Query("select a from Activity a where a.title = ?1")
+    List<Activity> getActivitiesByTitle(String title);
 }
