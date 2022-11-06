2cd959a1e53ba23ad373ffad8dd5aa2aab71ce84
Yanis Amrouche <54064644+yanisamrouche@users.noreply.github.com>
2022-11-06 18:12:44 +0100

    API Rest sur les personnes

--- src/main/java/myboot/app1/dao/PersonRepository.java
+++ src/main/java/myboot/app1/dao/PersonRepository.java
@@ -2,13 +2,20 @@ package myboot.app1.dao;
 
 import myboot.app1.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
-public interface PersonRepository extends JpaRepository<Person,Long> {
+public interface PersonRepository extends CrudRepository<Person,Long> {
 
+    @Query("select p from Person p where p.firstName = ?1")
+    List<Person> getPersonByFirstName(String firstName);
+    @Query("select p from Person p where p.lastName = ?1")
+    List<Person> getPersonByLastName(String lastName);
 }
 
