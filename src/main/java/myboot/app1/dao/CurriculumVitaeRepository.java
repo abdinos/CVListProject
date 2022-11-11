4eadd3d9c088a692b5d36b9f0e350e12e8669159
a18013526 <yann.amrouche@gmail.com>
2022-11-11 17:01:29 +0100

    adding a search bar for CVs

--- src/main/java/myboot/app1/dao/CurriculumVitaeRepository.java
+++ src/main/java/myboot/app1/dao/CurriculumVitaeRepository.java
@@ -1,14 +1,18 @@
 package myboot.app1.dao;
 
+import myboot.app1.model.Activity;
 import myboot.app1.model.CurriculumVitae;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface CurriculumVitaeRepository extends JpaRepository<CurriculumVitae, Integer> {
-
+    @Query("select c from CurriculumVitae c where c.cvName LIKE ?1")
+    List<CurriculumVitae> getCurriculumVitaeByName(String cvName);
 }
